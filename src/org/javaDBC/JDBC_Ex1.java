d210ae2ef5bf61f720f05aeb73a19a95d542026b
meshrammonali73 <meshrammonali73@gmail.com>
2022-03-25 10:00:49 +0530

    first jdbc

---
+++ src/org/javaDBC/JDBC_Ex1.java
@@ -0,0 +1,38 @@
+package org.javaDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class JDBC_Ex1 {
+
+	public static void main(String[] args) {
+		 Connection conn=null; 
+	       PreparedStatement pst=null;
+	       ResultSet rs=null;
+	       
+	       try {
+	    	   Class.forName("oracle.jdbc.driver.OracleDriver");
+	    	   conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","monali","monali");
+	    	   String sql="select * from emp";
+	    	   pst=conn.prepareStatement(sql);
+	    	   rs=pst.executeQuery();
+	    	   
+	    	   while(rs.next()) 
+	    	   {
+	    		 System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)); 
+	    		 //conn.close();
+	    	   }
+	       }
+	    	   catch(Exception e) 
+	    	   {
+	    		 System.out.println(e); 
+	    	   }
+	       
+		 }
+
+
+	}
+
+
